7530b07bb02bd3ade0f2383e80eff5778c6b4492
mrjokeller <jonikeller98@gmail.com>
2024-05-17 13:44:46 +0700

    Initial Commit

---
+++ MapView/MapViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewApp.swift
+//  MapView
+//
+//  Created by Jonathan Keller on 17.05.24.
+//
+
+import SwiftUI
+
+@main
+struct MapViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
